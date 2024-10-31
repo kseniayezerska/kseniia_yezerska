da062440cfbca83f21b2b6143f493fb60fe00dd8
kseniiayezerska <galat.k@icloud.com>
2024-10-31 20:53:55 +0200

    jshomework7

--- index.js
+++ index.js
@@ -1,65 +1,8 @@
-const company = {
-  name: 'Велика Компанія',
-  type:'Головна компанія',
-  platform: 'Платформа для продажу квитків',
-  sellsSolution: 'Рішення для продажу квитків',
-  clients: [
-    {
-      name: 'Клієнт 1',
-      type: 'subCompany',
-      uses: 'ПО для продажу квитків',
-      sells: 'Рішення для продажу квитків',
-      partners: [
-        {
-          name: 'Клієнт 1.1',
-          type: 'subSubCompany',
-          uses: 'Рішення для продажу квитків',
-          sells: 'Рішення для продажу квитків',
-        },
-        {
-          name: 'Клієнт 1.2',
-          type: 'subSubCompany',
-          uses: 'Рішення для продажу квитків',
-          sells: 'Рішення для продажу квитків',
-          partners: [
-            {
-              name: 'Клієнт 1.2.3',
-              type: 'subSubCompany',
-              uses: 'Рішення для продажу квитків',
-              sells: 'Рішення для продажу квитків',
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: 'Клієнт 2',
-      type: 'subCompany',
-      uses: 'ПО для продажу квитків',
-      sells: 'Рішення для продажу квитків'
-    }
-  ]
- };
- function findValueByKey(companyObj, companyName) {
-  if (companyObj.name === companyName) {
-    return companyObj;
-  }
-  if (companyObj.clients) {
-    for (const client of companyObj.clients) {
-      const result = findValueByKey(client, companyName);
-      if (result) return result;
-    }
-  }
-  if (companyObj.partners) {
-    for (const partner of companyObj.partners) {
-      const result = findValueByKey(partner, companyName);
-      if (result) return result;
-    }
-  }
-  return null;
- }
- const result = findValueByKey(company, 'Клієнт 1.2.3');
- console.log(result);
+const text = "Wonderful Joyful Happiness Time Task Apple";
+const regex = /\b[^aA\s]{6,}\b/g;
+const matches = text.match(regex);
+console.log(matches);
+
 
 
 
